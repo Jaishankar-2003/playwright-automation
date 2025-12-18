fb3554a62fe4f9065c0e8588bdfe862f15a00392
jaishankar-2003 <jaishankar98421@gmail.com>
2025-12-18 22:51:05 +0530

    test

--- pw demo/playwright.config.ts
+++ pw demo/playwright.config.ts
@@ -1,3 +1,5 @@
+// @ts-nocheck
+/// <reference types="node" />
 import { defineConfig, devices } from '@playwright/test';
 
 /**
@@ -15,12 +17,12 @@ export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  ...(process.env.CI ? { workers: 1 } : {}),
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
