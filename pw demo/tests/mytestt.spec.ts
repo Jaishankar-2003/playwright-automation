399ffbfec9d6f6cacae980e9d08cc57ba9d60d88
jaishankar-2003 <jaishankar98421@gmail.com>
2025-12-20 18:33:21 +0530

    test

---
+++ pw demo/tests/mytestt.spec.ts
@@ -0,0 +1,23 @@
+
+import { test ,expect } from '@playwright/test';
+
+// test("title ",()=>{
+//     //step 1: 
+//     //step 2:
+//     //step 3: 
+// })
+
+// fixture - global variable : page, browser ,context
+
+test("verify page title",async ({page})=>
+{
+    await page.goto("https://www.webpagetest.org/"); 
+
+    let title : string = await page.title();
+    console.log("Title :",title);
+
+
+    //await expect(page).toHaveTitle("catchpoint.");
+    
+})
+
