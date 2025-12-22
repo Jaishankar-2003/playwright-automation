5969d35b7834071f11bb28b4719ede87b7857cb5
jaishankar-2003 <jaishankar98421@gmail.com>
2025-12-22 22:33:14 +0530

    test

--- tests/pw-buildin-locator.spec.ts
+++ tests/pw-buildin-locator.spec.ts
@@ -1,8 +1,8 @@
 /*
 
-page.getByRole() to locate by explicit and implicit accessibility attributes.
+page.getByRole() to locate by explicit and implicit accessibility attributes.(button , link , checkbox ,textbox ,heading ,table)
 
-page.getByText() to locate by text content.  east to get value dont have attribute 
+page.getByText() to locate by text content.  east to get value dont have attribute (non intractive element)
 
 page.getByLabel() to locate a form control by associated label's text.
 
@@ -37,10 +37,28 @@ await expect(logo).toBeVisible();
 await expect(page.getByText('Welcome to our store')).toBeVisible(); // it work even it is full string ,substring , regular expression also  case sensitive
 
 
+// 3 page.getbyrole() locate by role (role is not an attribute)
+//role locators include button , checkbox , links , lists , headings , textbox ,table
+
+
+await page.getByRole('link', {name: 'Register'}).click();
+await expect(page.getByRole('heading' , {name : 'Register'})).toBeVisible();
+
+await page.getByLabel('First name:').fill("test");
+await page.getByLabel('Last name:').fill("user");
+await page.getByLabel('Email:').fill("test@gmail.com");
 
 
 
 
+// 5 page.getbyplaceholder() - find element by placeholder text (best for input without label but have a placeholder)
+
+await page.getByPlaceholder("Search store").fill("apple macbook pro");
+
+// page.getbytitle() to locate an element by its title attribute
+// when element have an meaning full title attribute 
+
+//let link:Locator = page.getByTitle("Computers")
 
 
 
