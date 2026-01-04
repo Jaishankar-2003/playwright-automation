816e1ab6f539dd7e046bf09fd9a28d081a49112f
jaishankar-2003 <jaishankar98421@gmail.com>
2026-01-04 22:20:41 +0530

    xpath

---
+++ tests/xpathaxes.spec.ts
@@ -0,0 +1,43 @@
+import { test ,expect, Locator } from '@playwright/test';
+
+test("xpath axes demo" , async ({page}) => {
+
+
+await page.goto("https://www.w3schools.com/html/html_tables.asp")
+
+// 1 self
+
+const gernamycell :  Locator = page.locator("//td[text()='Germany']/self::td");
+
+    console.log(await gernamycell.textContent());
+
+gernamycell.getByText
+//.toHaveText()
+
+// 2 parent
+
+const parentrow :  Locator = page.locator("//td[text()='Germany']/parent::tr");
+await expect(parentrow).toContainText("Maria Anders");
+console.log(await parentrow.textContent());
+
+
+// child 
+const child :  Locator = page.locator("//table[@id='customers']//tr[2]/child::td");
+expect(child).toHaveCount(3);
+
+
+// ancestor ( parent and grant parents)
+
+const table :  Locator = page.locator("//td[text() = 'Germany']/ancestor::table");
+
+await expect(table).toHaveAttribute('id' , 'customers')
+
+// descendent ( child and grant child)
+
+const decendent :  Locator = page.locator("//table[@id='customers']/descendant::td");
+
+
+// following  //td[normalize-space()='Germany']/following::td
+
+
+});
