8b0d8798e696ecaa56fb1b9e6ba6f486f04c7612
jaishankar-2003 <jaishankar98421@gmail.com>
2026-01-08 23:08:37 +0530

    test

--- tests/pwactions.spec.ts
+++ tests/pwactions.spec.ts
@@ -9,29 +9,29 @@ test("Actions" , async ({page}) => {
 
     await page.goto("https://testautomationpractice.blogspot.com/");
 
-    await page.waitForTimeout(2000);
+//     await page.waitForTimeout(2000);
 
-   const textbox : Locator =  page.locator('#name');
+//    const textbox : Locator =  page.locator('#name');
 
-   await expect(textbox).toBeVisible();
-    await expect(textbox).toBeEnabled();
+//    await expect(textbox).toBeVisible();
+//     await expect(textbox).toBeEnabled();
 
-    console.log(await textbox.getAttribute("maxlength"));
+//     console.log(await textbox.getAttribute("maxlength"));
 
-    await textbox.fill("hello world");
-
-
-
-
-    const entervalue : string = await textbox.inputValue();
-
-    console.log( "text content of first " + entervalue);  
-
-    expect(entervalue).toBe("hello world");
+//     await textbox.fill("hello world");
 
 
 
-     await page.waitForTimeout(3000);
+
+//     const entervalue : string = await textbox.inputValue();
+
+//     console.log( "text content of first " + entervalue);  
+
+//     expect(entervalue).toBe("hello world");
+
+
+
+//      await page.waitForTimeout(3000);
 
 
      
@@ -40,6 +40,7 @@ test("Actions" , async ({page}) => {
     console.log(await radio.isChecked());
 
 
+    // select specific 1 
 
 
     await page.locator("//input[@id='male']").check();
@@ -50,6 +51,67 @@ test("Actions" , async ({page}) => {
     await page.waitForTimeout(3000);
 
     await page.locator("//input[@id='monday']").check();
+
+    const mondaycheck = page.locator('#monday');
+
+
+     console.log(await mondaycheck.isChecked());
+
+    expect(await mondaycheck.isChecked()).toBe(true);
+
+
+    const sundaycheck = page.getByLabel('sunday');
+
+    await sundaycheck.check();
+
+    await expect(sundaycheck).toBeChecked();
+
+    console.log(await sundaycheck.isChecked());
+
+
+
+
+   const week = page.locator('input[type="checkbox"].form-check-input');
+
+
+
+   // select all checkboxes
+   for(const day of await week.all())
+   {
+        await day.check();
+        await expect(day).toBeChecked();
+   }
+
+
+   for(const checkweek of await week.all())
+   {
+        if(await checkweek.isChecked())
+        {
+            await checkweek.uncheck();
+        }
+        else
+        {
+            await checkweek.check();
+        }
+   }
+
+
+   // un check the last 3 check boxes
+   const lastThreeDays = (await week.all()).slice(-3);
+
+    for (const day of lastThreeDays) 
+    {
+        await day.uncheck();
+        await expect(day).not.toBeChecked();
+    }
+    
+
+    await page.waitForTimeout(3000);
+
+
+
+
+
 
 
 
